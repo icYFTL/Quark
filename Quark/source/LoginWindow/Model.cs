b0d10a604093a84897801a99487fbcf06ba3d0ea
icYFTL <savap0@yandex.ru>
2020-02-15 04:24:34 +0300

    Pre-footage

---
+++ Quark/source/LoginWindow/Model.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+
+namespace Quark.source.LoginWindow
+{
+    class Model : BindableBase { 
+
+        private readonly ObservableCollection<string> _groupItems = new ObservableCollection<string>();
+        public readonly ReadOnlyObservableCollection<string> GroupItems;
+
+        private readonly ObservableCollection<string> _studentItems = new ObservableCollection<string>();
+        public readonly ReadOnlyObservableCollection<string> StudentItems;
+        public Model()
+        {
+            StudentItems = new ReadOnlyObservableCollection<string>(_studentItems);
+            GroupItems = new ReadOnlyObservableCollection<string>(_groupItems);
+        }
+
+        public void UpdateGroups(List<string> _data)
+        {
+            foreach (var _t in _data)
+                _groupItems.Add(_t);
+
+            RaisePropertyChanged("GroupItems");
+        }
+
+        public void UpdateStudents(List<string> _data)
+        {
+            foreach (var _t in _data)
+                _studentItems.Add(_t);
+
+            RaisePropertyChanged("StudentItems");
+        }
+    }
+}
---
+++ Quark/source/LoginWindow/ViewModel.cs
@@ -0,0 +1,77 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.ObjectModel;
+using Quark.source.Utils.Database;
+using Prism.Mvvm;
+using System.Windows.Controls;
+using FirstFloor.ModernUI.Windows.Controls;
+
+namespace Quark.source.LoginWindow
+{
+    class ViewModel : BindableBase
+    {
+        readonly Model _model = new Model();
+        
+        public ViewModel()
+        {
+            _model.PropertyChanged += (s, e) => { RaisePropertyChanged(e.PropertyName); };
+
+            GroupsItemsSelectionChanged = new DelegateCommand<string>(str =>
+            {
+                if (GroupItems.Contains(str))
+                    UpdateStudents(str);
+            });
+            PasswordField_GotFocus = new DelegateCommand<PasswordBox>(pbox =>
+            {
+                if (pbox.Password == "Password")
+                    pbox.Password = "";
+            });
+
+            PasswordField_LostFocus = new DelegateCommand<PasswordBox>(pbox =>
+            {
+                if (pbox.Password == "")
+                    pbox.Password = "Password";
+            });
+
+            LoginCommand = new DelegateCommand<Object []>(obj =>
+            {
+                Login(obj);
+
+            });
+
+            UpdateGroups();
+        }
+        
+        public void UpdateGroups()
+        {
+            InternalBD _bd = InternalBD.getInstance();
+            _model.UpdateGroups(_bd.GetGroups());
+
+        }
+
+        public void UpdateStudents(string group)
+        {
+            InternalBD _bd = InternalBD.getInstance();
+            _model.UpdateStudents(_bd.GetStudents(group));
+        }
+
+        public void Login(Object [] obj)
+        {
+            // TO DO
+            MainWindow main = new MainWindow();
+            main.Show();
+            (obj[1] as ModernWindow).Close();
+        }
+
+
+    
+        public DelegateCommand<string> GroupsItemsSelectionChanged { get; }
+        public DelegateCommand<PasswordBox> PasswordField_GotFocus { get; }
+        public DelegateCommand<PasswordBox> PasswordField_LostFocus { get; }
+        public DelegateCommand<Object []> LoginCommand { get; }
+
+        public ReadOnlyObservableCollection<string> GroupItems => _model.GroupItems;
+        public ReadOnlyObservableCollection<string> StudentItems => _model.StudentItems;
+
+    }
+}
